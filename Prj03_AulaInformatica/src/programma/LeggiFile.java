57bad3434788e2925d9067f7a035c0637188bd0c
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2022-03-04 17:40:29 +0100

    agg

---
+++ Prj03_AulaInformatica/src/programma/LeggiFile.java
@@ -0,0 +1,35 @@
+package programma;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LeggiFile {
+
+	public static Studente[] studenti() {
+		
+		Studente[] contenitore = new Studente[27];
+		File f = new File("files/studenti.txt");
+		
+		try {//prova
+			Scanner sc = new Scanner(f);
+			int matr = 1;
+			while(sc.hasNextLine()) {
+				String riga =   sc.nextLine();
+				String[] parole = riga.split(",");
+				String nome = parole[0];
+				String cognome = parole[1];
+				
+				Studente s = new Studente(matr++, nome, cognome);
+				contenitore[matr -2] = s;
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return contenitore;
+	}
+	
+}
