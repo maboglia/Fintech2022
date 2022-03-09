60ef512471dd56259b3bd9d457988eed62a7c3a0
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2022-03-09 17:23:07 +0100

    agg

--- Prj05_TodoList/src/view/Demo.java
+++ Prj05_TodoList/src/view/Demo.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import controller.TodoCtrl;
 import model.Todo;
@@ -14,15 +15,50 @@ public class Demo {
 		
 		TodoCtrl ctrl = new TodoCtrl();
 		
-		ctrl.addTodo("latte");
-		ctrl.addTodo("pane");
-		ctrl.addTodo("vino");
-		ctrl.addTodo("biscotti");
-		ctrl.addTodo(new Todo("patatine"));
+		String scelta = null;
+		boolean gira = true;
 		
-		//ctrl.stampaElenco();
-		System.out.println(  ctrl.stampaHTML() );
+		while(gira) {
+			
+			menu();
+			scelta = chiediUtente();
+			
+			switch (scelta) {
+			case "1": 
+				System.out.println("aggiungi todo");
+				String cosa = cosaFare();
+				ctrl.addTodo(cosa);
+				break;
+			case "2": 
+				System.out.println("stampa todo");
+				ctrl.stampaElenco();
+				break;
+			case "3": 
+				System.out.println("salva file");
+				printer(ctrl);
+				break;
+			case "0": 
+				gira = false;
+				break;
+				
+			default:
+				System.out.println("scelta non disponibile");
+				break;
+			}
+			
+			
+			
+		}
 		
+	}
+
+	private static String cosaFare() {//metodo ausiliario
+		System.out.println("Aggiungi una voce all'elenco");
+		Scanner sc = new Scanner(System.in);
+		return sc.next();
+	}
+
+	private static void printer(TodoCtrl ctrl) {
 		try {
 			PrintStream ps = new PrintStream(new File("lista.html"));
 			ps.println(ctrl.stampaHTML());
@@ -33,9 +69,21 @@ public class Demo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private static String chiediUtente() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("scegli");
 		
-		
-		
+		return sc.next();
+	}
+
+	private static void menu() {
+		System.out.println("Benvenut* nella todo list");
+		System.out.println("1) aggiungi todo");
+		System.out.println("2) elenco todo");
+		System.out.println("3) stampa todo in html");
+		System.out.println("0) esci");
 	}
 
 }
