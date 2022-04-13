306061d7afa87f3404ec9101d50bca64623c47e3
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2022-04-13 15:55:28 +0200

    agg

---
+++ Prj02_Laboratorio/src/interfacce/MiaInterfaccia.java
@@ -0,0 +1,11 @@
+package interfacce;
+
+public interface MiaInterfaccia {
+	
+	//tutte le prop sono implicitamente public e final
+	String SALUTO = "ciao";
+	
+	String saluta();
+	String saluta(String s);
+
+}
